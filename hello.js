0b6ca540a70061e581fabcb0f21ef4cf1783aff5
Kourosh <picorog83@gmail.com>
2024-12-20 01:18:50 +0330

    some minor changes
    changed the visuals a bit
    also added the gameLoop to hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,22 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 350;
+canvas.width = 300;
 canvas.height = 600;
+
+function drawGrid() {
+    for(let x = 0;x < canvas.width;x += 30){
+        for(let y = 0;y < canvas.height;y += 30){
+            ctx.strokeStyle = '#333';
+            ctx.strokeRect(x,y,30,30);
+        }
+    }
+}
+
+function gameLoop(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawGrid();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
