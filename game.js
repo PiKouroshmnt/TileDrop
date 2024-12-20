33d7ce8e321fdd58e74fc8cebc42d4c84a2e9a70
Kourosh <picorog83@gmail.com>
2024-12-20 23:36:57 +0330

    fail condition and next block
    added fail condition and game over screen with
    restart function. also added next block mechanism.
    
    did some refactoring too. dont know why it took me this long
    to add an initializer function :|

--- game.js
+++ game.js
@@ -7,6 +7,9 @@ document.addEventListener('keydown', (event) => {
         rotateBlock();
     }else if(event.key === 'ArrowDown' && event.repeat){
         speedFall();
+    }else if(event.key === 'r' && isGameOver){
+        init();
+        gameLoop();
     }
 });
 
@@ -18,6 +21,9 @@ document.addEventListener('keyup', (event) =>{
 
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
+
+const nxtCanvas = document.getElementById('nextCanvas');
+const nxtCtx = nxtCanvas.getContext('2d');
 
 const SHAPES = [
     [[0,1],[1,1],[0,1]],   // T shape
@@ -46,27 +52,52 @@ const BLOCK_SIZE = 30;
 const GAME_WIDTH = canvas.width / BLOCK_SIZE;
 const GAME_HEIGHT = canvas.height / BLOCK_SIZE;
 
-let grid = Array.from({length: GAME_HEIGHT}, () =>
-    Array.from(({length: GAME_WIDTH}),() =>({
-        isOccupied : 0,
-        color : null,
-    }))
-);
+const NEXT_WIDTH = nxtCanvas.width / BLOCK_SIZE;
+const NEXT_HEIGHT = nxtCanvas.height / BLOCK_SIZE;
+
+let isGameOver;
+let gameLoopId;
+
+let grid;
 
 console.log(grid);
 
-let firstIndex = randomIndex(SHAPES.length);
-
 // the current block we want to draw
-let currentBlock = {
-    shape: SHAPES[firstIndex],
-    x: (GAME_WIDTH / 2) - 1,
-    y: 0,
-    color: COLORS[firstIndex],
-}
+let currentBlock;
 
-let lastDropTime = 0;
-let drop_delay = 1000;
+
+let nextBlock;
+
+let lastDropTime;
+let drop_delay;
+
+function init(){
+    isGameOver = false;
+    grid = Array.from({length: GAME_HEIGHT}, () =>
+        Array.from(({length: GAME_WIDTH}),() =>({
+            isOccupied : 0,
+            color : null,
+        }))
+    );
+    nxtCtx.clearRect(0,0,nxtCanvas.width,nxtCanvas.height);
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    let firstIndex = randomIndex(SHAPES.length);
+    let secondIndex = randomIndex(SHAPES.length);
+    currentBlock = {
+        shape: SHAPES[firstIndex],
+        x: (GAME_WIDTH / 2) - 1,
+        y: 0,
+        color: COLORS[firstIndex],
+    };
+    nextBlock = {
+        shape: SHAPES[secondIndex],
+        x: (NEXT_WIDTH / 2) - 1,
+        y: (NEXT_HEIGHT / 2) - 1,
+        color: COLORS[secondIndex],
+    }
+    lastDropTime = 0;
+    drop_delay = 1000;
+}
 
 function drawGrid() {
     for(let y = 0;y < GAME_HEIGHT;y++){
@@ -80,6 +111,15 @@ function drawGrid() {
                 ctx.strokeStyle = '#d3d3d3';
                 ctx.strokeRect(x * BLOCK_SIZE,y * BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE);
             }
+        }
+    }
+}
+
+function drawNextGrid() {
+    for(let y = 0;y < NEXT_HEIGHT;y++) {
+        for(let x = 0;x < NEXT_WIDTH;x++){
+            nxtCtx.strokeStyle = "#d3d3d3";
+            nxtCtx.strokeRect(x * BLOCK_SIZE,y * BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE);
         }
     }
 }
@@ -102,7 +142,30 @@ function drawBlock(block) {
                     (block.y + rowIndex) * BLOCK_SIZE,
                     BLOCK_SIZE,
                     BLOCK_SIZE
-                )
+                );
+            }
+        });
+    });
+}
+
+function drawNextBlock(){
+    nextBlock.shape.forEach((row,rowIndex) => {
+        row.forEach((cell,colIndex) => {
+            if(cell){
+                nxtCtx.strokeStyle = '#000';
+                nxtCtx.fillStyle = nextBlock.color;
+                nxtCtx.fillRect(
+                    (nextBlock.x + colIndex) * BLOCK_SIZE,
+                    (nextBlock.y + rowIndex) * BLOCK_SIZE,
+                    BLOCK_SIZE,
+                    BLOCK_SIZE,
+                );
+                nxtCtx.strokeRect(
+                    (nextBlock.x + colIndex) * BLOCK_SIZE,
+                    (nextBlock.y + rowIndex) * BLOCK_SIZE,
+                    BLOCK_SIZE,
+                    BLOCK_SIZE,
+                );
             }
         });
     });
@@ -168,11 +231,21 @@ function randomIndex(arrayLength) {
 
 function newBlock(){
     let index = randomIndex(SHAPES.length);
-    currentBlock = {
+    currentBlock = nextBlock;
+    currentBlock.x = (GAME_WIDTH / 2) - 1;
+    currentBlock.y = (-1 * currentBlock.shape.length) + 1;
+    nextBlock = {
         shape: SHAPES[index],
-        x : (GAME_WIDTH / 2) - 1,
-        y : 0,
+        x: (NEXT_WIDTH / 2) - 1,
+        y: (NEXT_HEIGHT / 2) - 1,
         color: COLORS[index],
+    }
+}
+
+function spawnNewBlock() {
+    newBlock();
+    if(nextPosition(currentBlock,currentBlock.y,currentBlock.x) === null){
+        gameOver();
     }
 }
 
@@ -190,7 +263,8 @@ function placeBlockOnGrid() {
             if (cell) {
                 const x = currentBlock.x + colIndex;
                 const y = currentBlock.y + rowIndex;
-
+                if(y < 0)
+                    return;
                 grid[y][x].isOccupied = 1;
                 grid[y][x].color = currentBlock.color;
             }
@@ -211,8 +285,12 @@ function nextPosition(block,newY,newX) {
             if(cell) {
                 const x = nextBlock.x + colIndex;
                 const y = nextBlock.y + rowIndex;
-
-                if(y >= GAME_HEIGHT || y < 0 || x >= GAME_WIDTH || x < 0 || grid[y][x].isOccupied === 1){
+                
+                if(y < 0) {
+                    return;
+                }
+                
+                if(y >= GAME_HEIGHT || x >= GAME_WIDTH || x < 0 || grid[y][x].isOccupied === 1){
                     isnull = true;
                 }
             }
@@ -258,20 +336,52 @@ function gravity(block) {
         block.y++;
     }else{
         placeBlockOnGrid();
-        newBlock();
+        spawnNewBlock();
         clearFullLines();
     }
 }
 
 function gameLoop(currenTime){
+    if(isGameOver) return;
+    nxtCtx.clearRect(0,0,nxtCanvas.width,nxtCanvas.height);
     ctx.clearRect(0,0,canvas.width,canvas.height);
     drawGrid();
+    drawNextGrid();
     drawBlock(currentBlock);
+    drawNextBlock();
     if(currenTime - lastDropTime > drop_delay){
        gravity(currentBlock);
        lastDropTime = currenTime;
     }
-    requestAnimationFrame(gameLoop);
+    gameLoopId = requestAnimationFrame(gameLoop);
 }
 
+function gameOver(){
+    isGameOver = true;
+
+    cancelAnimationFrame(gameLoopId);
+
+    nxtCtx.clearRect(0,0,nxtCanvas.width,nxtCanvas.height);
+
+    ctx.fillStyle = "rgba(255,255,255,0.5)";
+    nxtCtx.fillStyle = "rgba(255,255,255,0.5)";
+
+    let offset = 50;
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = "rgba(0,0,0,0.6)";
+    ctx.fillRect(0,(canvas.height / 2) - offset,canvas.width,2 * offset);
+
+    nxtCtx.fillRect(0,0,nxtCanvas.width,nxtCanvas.height);
+
+    ctx.fillStyle = "rgb(255,0,0)";
+    ctx.font = "30px Arial";
+    ctx.textAlign = "center";
+    ctx.fillText("Game over",canvas.width / 2,canvas.height / 2);
+
+    ctx.fillStyle = "#d3d3d3";
+    ctx.font = "15px Arial";
+    ctx.fillText("press R to restart",canvas.width / 2,(canvas.height / 2) + 20);
+}
+
+init();
 gameLoop();
